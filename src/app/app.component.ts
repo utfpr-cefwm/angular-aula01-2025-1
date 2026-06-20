7f89c8dbfca2e41f60e730b7ca33ffc885812a1d
Rui Leite <ruipimentelleite@gmail.com>
2026-06-20 16:31:36 -0300

    Implementa a listagem de planos.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ConsultaSaldoComponent } from "./components/consulta-saldo/consulta-saldo.component";
+import { ListaContratosComponent } from "./components/lista-contratos/lista-contratos.component";
 import { Contrato } from './models/contrato';
 
 @Component({
@@ -9,13 +10,26 @@ import { Contrato } from './models/contrato';
   imports: [
     RouterOutlet,
     ConsultaSaldoComponent,
+    ListaContratosComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  protected contrato: Contrato = {
-    nome: 'Plano Plus',
-    cor: 'azul',
-  }
+  protected contrato?: Contrato;
+
+  protected contratos: Contrato[] = [
+    {
+      nome: 'Plano Light',
+      cor: 'branco',
+    },
+    {
+      nome: 'Plano Plus',
+      cor: 'azul',
+    },
+    {
+      nome: 'Plano TotalZord',
+      cor: 'preto',
+    },
+  ];
 }
--- src/app/components/lista-contratos/lista-contratos.component.ts
+++ src/app/components/lista-contratos/lista-contratos.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Contrato } from '../../models/contrato';
 
 @Component({
   selector: 'app-lista-contratos',
   standalone: true,
   imports: [
+    CommonModule,
   ],
   templateUrl: './lista-contratos.component.html',
   styleUrl: './lista-contratos.component.css',
 })
 export class ListaContratosComponent {
+
+  @Input({
+    required: true,
+  })
+  public contratosPossiveis!: Contrato[];
 
 }
